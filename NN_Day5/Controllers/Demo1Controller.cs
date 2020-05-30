651d6753988baf4bc78aa2ba48ae16e7c28cfa62
NhatNghe_Day06_Route <truong.nguyennhat1997@gmail.com>
2020-05-30 15:37:29 +0700

    Update 1

--- NN_Day5/Controllers/Demo1Controller.cs
+++ NN_Day5/Controllers/Demo1Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NN_Day6_Route.Models;
 
 namespace NN_Day5.Controllers
 {
@@ -12,8 +13,16 @@ namespace NN_Day5.Controllers
         {
             ViewBag.NgayThanhLap = new DateTime(2003, 3, 10);
             ViewBag.Ten = "Nhất Nghệ";
+
+            //ViewData nhận [key value]:
             ViewData["Tuoi"] = 17;
-            ViewData["Phone"] = 0707459367;
+            ViewData["Phone"] = "0707459367";
+
+            ViewBag.SanPham = new HangHoa
+            {
+                MaHH = 1, TenHH = "Bia Sài Gòn",
+                DonGia = 18900, GiamGia = 3
+            };
             return View();
         }
 
