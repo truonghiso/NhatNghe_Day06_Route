17e2448bd292259455b4fc3e855087410d4e0643
NhatNghe_Day06_Route <truong.nguyennhat1997@gmail.com>
2020-05-30 17:23:07 +0700

    Update 2

---
+++ NN_Day5/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NN_Day6_Route.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(double ToanHang1, double ToanHang2, string PhepToan)
+        {
+            double kq = 0;
+            switch (PhepToan)
+            {
+                case "+": kq = ToanHang1 + ToanHang2; break;
+                case "-": kq = ToanHang1 - ToanHang2; break;
+                case "*": kq = ToanHang1 * ToanHang2; break;
+                case "/": kq = ToanHang1 / ToanHang2; break;
+                case "^": kq = Math.Pow(ToanHang1, ToanHang2); break;
+            }
+
+            ViewBag.A = ToanHang1;
+            ViewBag.B = ToanHang2;
+            ViewBag.PhepToan = PhepToan;
+            ViewBag.KetQua = kq;
+
+            return View("Index");
+            //return Content($"{ToanHang1} {PhepToan} {ToanHang2} = {kq}");
+        }
+    }
+}
---
+++ NN_Day5/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NN_Day6_Route.Models;
+
+namespace NN_Day6_Route.Controllers
+{
+    public class ProductsController : Controller
+    {
+        static List<HangHoa> dsHangHoa = new List<HangHoa>();
+
+        public ProductsController()
+        {
+            dsHangHoa.Add(new HangHoa
+            {
+                MaHH = 1,
+                TenHH = "Bánh Mì",
+                DonGia = 45900,
+                GiamGia = 1
+            });
+            dsHangHoa.Add(new HangHoa
+            {
+                MaHH = 2,
+                TenHH = "Mì Xào Bò",
+                DonGia = 41000,
+                GiamGia = 5
+            });
+        }
+
+        public IActionResult Index()
+        {
+            return View(dsHangHoa);
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoa hanghoa)
+        {
+            dsHangHoa.Add(hanghoa);
+            return View ("Index", dsHangHoa);
+            //return Redirect("/HangHoa");
+            //return RedirectToAction("Index", "HangHoa");
+        }
+
+        public IActionResult Create()
+        {
+            return View(dsHangHoa);
+        }
+    }
+}
