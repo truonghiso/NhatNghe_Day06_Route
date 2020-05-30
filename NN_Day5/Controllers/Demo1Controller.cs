6283127f90609f60024359d08f9655aa7dd241c8
NhatNghe_Day06_Route <truong.nguyennhat1997@gmail.com>
2020-05-30 15:05:19 +0700

    Học về Route

---
+++ NN_Day5/Controllers/Demo1Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NN_Day5.Controllers
+{
+    public class Demo1Controller : Controller
+    {
+        public IActionResult TruyenDuLieu()
+        {
+            ViewBag.NgayThanhLap = new DateTime(2003, 3, 10);
+            ViewBag.Ten = "Nhất Nghệ";
+            ViewData["Tuoi"] = 17;
+            ViewData["Phone"] = 0707459367;
+            return View();
+        }
+
+        [Route("/dien-thoai/{tenDienThoai}")]
+
+        public IActionResult ChiTiet(string tenDienThoai)
+        {
+            return Content(tenDienThoai);
+        }
+
+        [Route("/{loai}/{sanpham}")]
+        public IActionResult ChiTiet(string loai, string sanpham)
+        {
+            return Content($"{loai} - {sanpham}");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NN_Day5/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NN_Day5.Models;
+
+namespace NN_Day5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Demo")]
+        [Route("/Vidu")]
+        [Route("[controller]/[action]")]
+
+        public IActionResult ABC()
+        {
+            return Content("Demo Route");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
